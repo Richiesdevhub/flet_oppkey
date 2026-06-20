165e37781cd9a0c587a20b30200392c4ca927c4b
Richiegithub <rislfagmail.com>
2026-06-20 13:27:46 -0600

    push con cambios de github codespaces

--- list_comp_003/src/main.py
+++ list_comp_003/src/main.py
@@ -7,9 +7,31 @@ def main(page: ft.Page):
     def create_cory():
         return ft.Image("cory_front.png", width=58, height=79)
 
-    corys = [create_cory() for _ in range(5)]
+    def icon_test():
+        return ft.IconButton(
+            icon=ft.Icons.ACCESS_ALARM,
+            icon_size=100,
+            bgcolor="green",
+            icon_color="white"
+        )
 
-    page.add(ft.Row(corys))
+    corys = [icon_test() for _ in range(5)]
+
+    cory = ft.Container(
+        content=ft.Image(src="cory_front.png", width=58, height=79),
+        left=0,
+        top=50
+    )
+
+    game_area = ft.Stack(
+        controls=[cory],
+        width=ft.Window.width,
+        height=300
+    )
+
+    # page.add(ft.Row(corys))
+    page.add(game_area)
+
 
 if __name__ == "__main__":
     ft.run(main, assets_dir="assets")
